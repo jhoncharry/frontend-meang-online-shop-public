e01f506ade3412847ed42eb6a5dc8ba0b36bf5c2
jhoncharry <jhon.charryserrato@gmail.com>
2021-11-26 20:25:41 -0500

    Some changes subscription env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   stripe_public_key:
     'pk_test_51JxDIrLHC9KMOZhNCVtz6mQo6BvVVPeQXSD40t1M4ochcOhlCkJz0SnpP6rou0wRvIGq7Jm1cNycst1WD2sJ2MO200LNmWxJgd',
   graphql_api: 'https://game-store-meang.herokuapp.com/graphql',
-  graphql_subscription_api: 'ws://game-store-meang.herokuapp.com/graphql',
+  graphql_subscription_api: 'wss://game-store-meang.herokuapp.com/graphql',
   admin_route: 'https://game-store-admin.netlify.app',
 };
